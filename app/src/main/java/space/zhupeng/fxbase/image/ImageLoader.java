a8ef37b3dea57f553907d81c73a090be63be1fac
zhupeng <1455832805@qq.com>
2017-09-13 17:06:22 +0800

    add some

--- app/src/main/java/space/zhupeng/fxbase/image/ImageLoader.java
+++ app/src/main/java/space/zhupeng/fxbase/image/ImageLoader.java
@@ -1,5 +1,10 @@
 package space.zhupeng.fxbase.image;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.widget.ImageView;
+
 /**
  * 屏蔽底层图像加载库
  *
@@ -7,5 +12,16 @@ package space.zhupeng.fxbase.image;
  * @date 2017/5/30
  */
 
-public class ImageLoader {
+public interface ImageLoader {
+
+    void displayImage(Context context,
+                      String path,
+                      ImageView imageView,
+                      Drawable defaultDrawable,
+                      Bitmap.Config config,
+                      boolean resize,
+                      boolean isGif,
+                      int width,
+                      int height,
+                      int rotate);
 }
