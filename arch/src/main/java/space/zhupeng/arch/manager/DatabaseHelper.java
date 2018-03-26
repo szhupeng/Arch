68714b009fcb1ef8c8e452abd8dca88127ce2048
zhupeng <1455832805@qq.com>
2018-03-26 11:28:25 +0800

    update

--- arch/src/main/java/space/zhupeng/arch/manager/DatabaseHelper.java
+++ arch/src/main/java/space/zhupeng/arch/manager/DatabaseHelper.java
@@ -1,6 +1,8 @@
 package space.zhupeng.arch.manager;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.HandlerThread;
 
 /**
  * @author zhupeng
@@ -13,9 +15,31 @@ public class DatabaseHelper {
     protected final String mDatabaseName;
     protected final int mDatabaseVersion;
 
+    private final HandlerThread mReadWriteThread;
+    private final Handler mReadWriteHandler;
+
     public DatabaseHelper(Context context, String dbName, int dbVersion) {
         this.context = context.getApplicationContext();
         this.mDatabaseName = dbName;
         this.mDatabaseVersion = dbVersion;
+
+        this.mReadWriteThread = new HandlerThread("ReadWriteDbThread");
+        this.mReadWriteThread.start();
+        this.mReadWriteHandler = new Handler(mReadWriteThread.getLooper());
+    }
+
+    public void postSafely(final Runnable runnable) {
+        if (runnable != null) {
+            this.mReadWriteHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        runnable.run();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
     }
 }
